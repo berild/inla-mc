038247144834d3b27da40561ef653ad5b2c378dc
berild <moutzebe@gmail.com>
2021-12-30 00:07:17 +0100

    fix plotting function for publication

--- toy/toy.R
+++ toy/toy.R
@@ -51,18 +51,28 @@ dq.beta <- function(y, theta = init, t = 1, log =TRUE) {
   dmvnorm(y,mean = theta[[1]], sigma = theta[[2]],log = log)
 }
 
+amis_mod = list(eta = amis_w_inla_mod$eta,times = amis_w_inla_mod$times, theta = amis_w_inla_mod$theta, weight = amis_w_inla_mod$weight,
+                margs = amis_w_inla_mod$margs, eta_kern = amis_w_inla_mod$eta_uni_kerns)
+
+is_mod = list(eta = is_w_inla_mod$eta,times = is_w_inla_mod$times, theta = is_w_inla_mod$theta, weight = is_w_inla_mod$weight,
+              margs = is_w_inla_mod$margs, eta_kern = is_w_inla_mod$eta_uni_kerns)
+
+mcmc_mod = list(eta = mcmc_w_inla_mod$eta,times = mcmc_w_inla_mod$times, acc.vec = mcmc_w_inla_mod$acc.vec, 
+                margs = mcmc_w_inla_mod$margs, eta_kern = mcmc_w_inla_mod$eta_uni_kerns)
+
+
 ### AMIS
 set.seed(1)
 amis_mod <- inlaAMIS(data = df, init = init, prior.beta,
                                dq.beta, rq.beta, fit.inla,
-                               N_t = seq(25,50)*10, N_0 = 250,ncores = 10)
-save(amis_mod, file = "./sims/toy/toy-amis.Rdata")
+                               N_t = seq(25,50)*10, N_0 = 250,ncores = 10,kde = T)
+save(amis_mod, file = "./sims/toy/amis_toy.Rdata")
 
 ### IS
 set.seed(1)
 is_mod <- inlaIS(data = df, init = init, prior.beta,
-                           dq.beta, rq.beta, fit.inla, N_0 = 800, N = 10000,ncores = 10)
-save(is_mod, file = "./sims/toy/toy-is.Rdata")
+                           dq.beta, rq.beta, fit.inla, N_0 = 800, N = 10000,ncores = 10,kde = T)
+save(is_mod, file = "./sims/toy/is_toy.Rdata")
 
 ### MCMC
 
@@ -78,5 +88,5 @@ dq.beta <- function(y, x, sigma = .75, log =TRUE) {
 init = list(mu = c(0,0),cov = 0.5*diag(2))
 set.seed(1)
 mcmc_mod <- inlaMH(data = df, init = init, prior.beta,
-                               dq.beta, rq.beta, fit.inla, n.samples = 10500, n.burnin = 500, n.thin = 1)
-save(mcmc_w_inla_mod, file = "./sims/toy/toy-mcmc.Rdata")
+                               dq.beta, rq.beta, fit.inla, n.samples = 10500, n.burnin = 500, n.thin = 1, kde=T)
+save(mcmc_mod, file = "./sims/toy/mcmc_toy.Rdata")
