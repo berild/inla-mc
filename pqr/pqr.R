ddc569e261e933783cc1a1f70f8dc9d897e47f8e
berild <moutzebe@gmail.com>
2021-12-31 15:04:25 +0100

    fix scaling of parameters

--- pqr/pqr.R
+++ pqr/pqr.R
@@ -94,18 +94,18 @@ set.seed(1)
 amis_mod = inlaAMIS(data = lidar, init = init, prior.param,
                               dq.param, rq.param, fit.inla.rw2,
                               N_t = seq(25,50,1)*10, N_0 = 250,ncores = 10)
-amis_w_inla_mod$mod = lidar
-amis_w_inla_mod$scale = c(r_max,r_min)
-save(amis_w_inla_mod, file = "./sims/pqr/amis_pqr_rw2.Rdata")
+amis_mod$mod = lidar
+amis_mod$scale = c(r_max,r_min)
+save(amis_mod, file = "./sims/pqr/amis_pqr_rw2.Rdata")
 
 # running is with inla on lidar data
 set.seed(1)
 is_mod = inlaIS(data = lidar, init = init, prior.param,
                           dq.param, rq.param, fit.inla.rw2,
                           N_0 = 800, N = 10000,ncores = 10)
-is_w_inla_mod$mod = lidar
-is_w_inla_mod$scale = c(r_max,r_min)
-save(is_w_inla_mod, file = "./sims/pqr/is_pqr_rw2.Rdata")
+is_mod$mod = lidar
+is_mod$scale = c(r_max,r_min)
+save(is_mod, file = "./sims/pqr/is_pqr_rw2.Rdata")
 
 # initalizing proposal distribution for mcmc with inla
 init_mcmc = list(mu = c(0,0),cov = diag(2))
@@ -121,56 +121,15 @@ set.seed(1)
 mcmc_mod <- inlaMH(data = lidar, init = init_mcmc,
                                prior.param, dq.param.mcmc, rq.param.mcmc, fit.inla.rw2,
                                n.samples = 10500, n.burnin = 500, n.thin = 1)
-save(mcmc_w_inla_mod, file = "./sims/pqr/mcmc_pqr_rw2.Rdata")
-eta_kern_mcmc = kde2d.weighted(x = mcmc_w_inla_mod$eta[,1], y = mcmc_w_inla_mod$eta[,2], w = rep(1,nrow(mcmc_w_inla_mod$eta))/nrow(mcmc_w_inla_mod$eta), n = 100, lims = c(-1,1,-1,1))
-mcmc_w_inla_mod$eta_kern_joint = data.frame(expand.grid(x=eta_kern_mcmc$x, y=eta_kern_mcmc$y), z=as.vector(eta_kern_mcmc$z))
-mcmc_w_inla_mod$eta_uni_kerns= lapply(seq(ncol(mcmc_w_inla_mod$eta)), function(x){
-  as.data.frame(density(x = mcmc_w_inla_mod$eta[,x],
-                        weights = rep(1/length(mcmc_w_inla_mod$eta[,x]),length(mcmc_w_inla_mod$eta[,x])),
+save(mcmc_mod, file = "./sims/pqr/mcmc_pqr_rw2.Rdata")
+eta_kern_mcmc = kde2d.weighted(x = mcmc_mod$eta[,1], y = mcmc_mod$eta[,2], w = rep(1,nrow(mcmc_mod$eta))/nrow(mcmc_mod$eta), n = 100, lims = c(-1,1,-1,1))
+mcmc_mod$eta_kern_joint = data.frame(expand.grid(x=eta_kern_mcmc$x, y=eta_kern_mcmc$y), z=as.vector(eta_kern_mcmc$z))
+mcmc_mod$eta_kerns= lapply(seq(ncol(mcmc_mod$eta)), function(x){
+  as.data.frame(density(x = mcmc_mod$eta[,x],
+                        weights = rep(1/length(mcmc_mod$eta[,x]),length(mcmc_mod$eta[,x])),
                         kernel = "gaussian")[c(1,2)])
 })
-mcmc_w_inla_mod$mod = lidar
-mcmc_w_inla_mod$scale = c(r_max,r_min)
-save(mcmc_w_inla_mod, file = "./sims/pqr-gaussian-lidar-mcmc-w-inla.Rdata")
+mcmc_mod$mod = lidar
+mcmc_mod$scale = c(r_max,r_min)
+save(mcmc_mod, file = "./sims/pqr/mcmc_pqr_rw2.Rdata")
 
-# ImmunogG data
-data("ImmunogG")
-# changing intial proposal distribution amis and is
-
-init = list(mu = c(2,0),cov = diag(c(1,0.5)))
-
-formula = IgG ~ f(Age, model = "rw2", constr = T, scale.model =  F)
-res = inla(formula,
-           data = ImmunogG,
-           control.predictor = list(compute = T),
-           family = "gamma",
-           verbose = FALSE)
-
-#running amis with inla on ImmunogG data
-amis_mod = inlaAMIS(data = ImmunogG,
-                    init = init, prior.param, dq.param, rq.param, fit.inla.rw2.gamma,
-                    N_t = seq(25,50,1), N_0 = 25,kde = T,ncores=10)
-save(amis_mod, file = "./pqr/pqr-gammarw2-ImmunogG-amis.Rdata")
-
-
-# running is with inla on ImmunogG data
-is_w_inla_mod = is.w.inla(data = list(x=ImmunogG$Age,y = ImmunogG$IgG),
-                         init = init, prior.param, dq.param, rq.param, fit.inla.ggamma, N_0 = 800,N = 10000)
-is_w_inla_mod$mod = list(x=ImmunogG$Age,y = ImmunogG$IgG)
-save(is_w_inla_mod, file = "./sims/pqr-gamma-ImmunogG-is-w-inla.Rdata")
-
-# running mcmc with inla on ImmunogG data
-init_mcmc = list(mu = c(0,0),cov = diag(c(0.3,0.15)))
-mcmc_w_inla_mod <- mcmc.w.inla(data = list(x=ImmunogG$Age,y = ImmunogG$IgG), init = init_mcmc,
-                              prior.param, dq.param.mcmc, rq.param.mcmc, fit.inla.ggamma,
-                              n.samples = 10500, n.burnin = 500, n.thin = 1)
-save(mcmc_w_inla_mod, file = "./sims/pqr-gamma-ImmunogG-mcmc-w-inla.Rdata")
-eta_kern_mcmc = kde2d.weighted(x = mcmc_w_inla_mod$eta[,1], y = mcmc_w_inla_mod$eta[,2], w = rep(1,nrow(mcmc_w_inla_mod$eta))/nrow(mcmc_w_inla_mod$eta), n = 100, lims = c(-1,1,-1,1))
-mcmc_w_inla_mod$eta_kern_joint = data.frame(expand.grid(x=eta_kern_mcmc$x, y=eta_kern_mcmc$y), z=as.vector(eta_kern_mcmc$z))
-mcmc_w_inla_mod$eta_uni_kerns= lapply(seq(ncol(mcmc_w_inla_mod$eta)), function(x){
- as.data.frame(density(x = mcmc_w_inla_mod$eta[,x],
-                       weights = rep(1/length(mcmc_w_inla_mod$eta[,x]),length(mcmc_w_inla_mod$eta[,x])),
-                       kernel = "gaussian")[c(1,2)])
-})
-mcmc_w_inla_mod$mod = list(x=ImmunogG$Age,y = ImmunogG$IgG)
-save(mcmc_w_inla_mod, file = "./sims/pqr-gamma-ImmunogG-mcmc-w-inla.Rdata")
